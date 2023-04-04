95a6cfbe16dbf8d6a80d33ecca947ee09d35b5e1
Sebastian <sebastiansloma@gmail.com>
2023-04-04 13:29:47 +0200

    import cards

---
+++ src/setup.ts
@@ -0,0 +1,10 @@
+import card1 from './img/cap.jpg'
+import card2 from './img/doom.jpg'
+import card3 from './img/hulk.jpg'
+import card4 from './img/ironman.jpg'
+import card5 from './img/pant.jpg'
+import card6 from './img/thor.jpg'
+import card7 from './img/venom.jpg'
+import card8 from './img/wolverine.jpg'
+
+import cardBack from './img/marvel.jpg'
