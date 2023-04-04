53fddb3b702ec8012fbada45afe6fbbe2262bea5
Sebastian <sebastiansloma@gmail.com>
2023-04-04 13:50:37 +0200

    added card logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react"
+import { Grid } from "./App.styles"
+import { shuffleArray } from "./utils"
+import { createBoard } from "./setup"
+import { CardType } from "./setup"
 
 function App() {
+  const [cards, setCards] = React.useState<CardType[]>(shuffleArray(createBoard()))
+  const[gameWon, setGameWon] = React.useState(false)
+  const[clickedCard, setClickedCard] = React.useState<undefined | CardType>(undefined)
   
 
   return (
     
        
   <div>
-<h2>hi there</h2>
+<Grid>
+  {cards.map(card =>(
+    <p>{card.id}</p>
+  ))}
+</Grid>
   </div>
   )
 }
--- src/setup.ts
+++ src/setup.ts
@@ -1,10 +1,31 @@
-import card1 from './img/cap.jpg'
-import card2 from './img/doom.jpg'
-import card3 from './img/hulk.jpg'
-import card4 from './img/ironman.jpg'
-import card5 from './img/pant.jpg'
-import card6 from './img/thor.jpg'
-import card7 from './img/venom.jpg'
-import card8 from './img/wolverine.jpg'
+import card1 from './img/cap.jpg';
+import card2 from './img/doom.jpg';
+import card3 from './img/hulk.jpg';
+import card4 from './img/ironman.jpg';
+import card5 from './img/pant.jpg';
+import card6 from './img/thor.jpg';
+import card7 from './img/venom.jpg';
+import card8 from './img/wolverine.jpg';
 
-import cardBack from './img/marvel.jpg'
+import cardBack from './img/marvel.jpg';
+
+export type CardType = {
+	id: string;
+	flipped: boolean;
+	backImage: string;
+	frontImage: string;
+	clickable: boolean;
+	matchingCardId: string;
+};
+
+const cards: string[] =[card1, card2, card3, card4, card5, card6, card7, card8]
+
+export const createBoard =(): CardType[] =>
+[...cards, ...cards].map((card, i) =>({
+    id: `card${i}`,
+    flipped: false,
+    backImage: cardBack,
+    frontImage: card,
+    clickable: true,
+    matchingCardId: i < cards.length ? `card${i + cards.length}` : `card${i - cards.length}`
+}))
